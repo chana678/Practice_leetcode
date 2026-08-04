d4a2b7494af64d77a988d9ad0b04000bde778d47
Chanakya Basumatari <chanakyabasumatari@gmail.com>
2026-08-04 07:02:40 +0530

    Some more practice on Sliding Window

--- 3.1Sliding_Window_Variable/01_Longest_Substring_Without_Repeating_Characters.py
+++ 3.1Sliding_Window_Variable/01_Longest_Substring_Without_Repeating_Characters.py
@@ -12,11 +12,15 @@ Medium
 
 My First Thought:
 
-
+How do I maintain a dynamic window and also at the same time continue iterating in the loop. I initially though of moving the pointers, but I couldn't
+formulate it. So a Python Set is an ideal choice to be a dynamic window, as it will grow and shrink in O(1) time based on the constraints.
 --------------------------------------------------
 
 Observation:
-
+We can use two pointer - left and right. The right pointer will be the optimistic one, and check for evry value in the list and likewise the window
+will grow, until a value is found that already exists in the window. Now since the constraint is violated we have to shrik - we increment the left
+pointer in the list and remove element from the window(set) until the repeating element is found and removed. Once the constraint is satisfied we 
+grow again and return the lenght of the longest substring without repeating characters.
 
 --------------------------------------------------
 
