95e503367b3a63978f3f75dac72c49963632525d
Chanakya Basumatari <chanakyabasumatari@gmail.com>
2026-08-12 06:55:16 +0530

    Practice on Binary Search

--- 4.Binary_Search/03_Search_Insert_Position.py
+++ 4.Binary_Search/03_Search_Insert_Position.py
@@ -47,3 +47,26 @@ a sorted list
 Mistakes:
 
 """
+
+def search_insert(nums, target):
+    left = 0
+    right = len(nums) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if nums[mid] == target:
+            return mid
+
+        elif nums[mid] < target:
+            left = mid + 1
+
+        elif nums[mid] > target:
+            right = mid - 1
+
+    return left
+
+nums = [1,3,5,6]
+target = 2
+
+print(search_insert(nums, target))
